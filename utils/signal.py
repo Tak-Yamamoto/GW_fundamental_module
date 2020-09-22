6129b8fa635363f3183742d6b01bf3c87d33f1f9
tak.yamamoto <tak.guruguru.4@gmail.com>
2020-09-22 12:19:05 +0900

    update 200922

--- utils/signal.py
+++ utils/signal.py
@@ -22,7 +22,7 @@ def get_whitenedstrain(strain, dt, psd):
     N = len(strain)
     freq = np.fft.rfftfreq(N, dt)
     df = abs(freq[1] - freq[0])
-    psdarray = psd(freq)
-    hf = np.fft.rfft(strain) * dt / psdarray
+    norm = 1./np.sqrt(1./(dt*2))
+    hf = np.fft.rfft(strain) / np.sqrt(psd(freq)) * norm
 
-    return np.fft.irfft(hf) * df
+    return np.fft.irfft(hf, n=N) * df
